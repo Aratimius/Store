6b552b7c9321b48a066a407f3145337e9c2be8b8
Aratimius <aratimius@gmail.com>
2024-02-21 18:38:15 +0300

    Add migrations Project and Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=100, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}: {self.description}'
+
+    class Meta:
+        verbose_name = 'наименование'
+        verbose_name_plural = 'описание'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название товара')
+    description = models.TextField(verbose_name='описание товара')
+    preview = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория товара')
+    price = models.IntegerField(verbose_name='цена товара')
+    created_at = models.DateTimeField(verbose_name='дата создания')
+    updated_at = models.DateTimeField(verbose_name='дата обновления')
+
+    def __str__(self):
+        return f'{self.name}: {self.description}'
+
+    class Meta:
+        verbose_name = 'подукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name', 'description',)
