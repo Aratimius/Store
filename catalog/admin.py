3153b8ea0ab6a0cc63afadfd3b7b282d6320f61c
Aratimius <aratimius@gmail.com>
2024-02-21 22:23:12 +0300

    Исправлена ошибка в оформлении при решении Задачи 5

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+# admin.site.register(Product)
+# admin.site.register(Category)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
--- catalog/models.py
+++ catalog/models.py
@@ -6,14 +6,14 @@ NULLABLE = {'null': True, 'blank': True}
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
-    description = models.CharField(max_length=100, verbose_name='описание')
+    description = models.TextField(max_length=100, verbose_name='описание')
 
     def __str__(self):
-        return f'{self.name}: {self.description}'
+        return f'{self.name}'
 
     class Meta:
-        verbose_name = 'наименование'
-        verbose_name_plural = 'описание'
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
         ordering = ('name',)
 
 
